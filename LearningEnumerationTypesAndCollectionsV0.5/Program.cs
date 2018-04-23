84caf46b470452b5a7b8d581e84d96e8e103ad54
Hajk <tunczorx@gmail.com>
2018-04-23 15:36:17 +0200

    List<T> and rest. Seems to be practical.

--- LearningEnumerationTypesAndCollectionsV0.5/Program.cs
+++ LearningEnumerationTypesAndCollectionsV0.5/Program.cs
@@ -8,8 +8,63 @@ namespace LearningEnumerationTypesAndCollectionsV0._5
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            List<Shoe> shoeCloset = new List<Shoe>();
+
+            shoeCloset.Add(new Shoe()
+            {
+                Style = Style.Sneakers,
+                Color = "Czarny"});
+
+            shoeCloset.Add(new Shoe()
+            {
+                Style = Style.Clogs,
+                Color = "Brązowy"});
+
+            shoeCloset.Add(new Shoe()
+            {
+                Style = Style.Wingtips,
+                Color = "Czarny"});
+
+            shoeCloset.Add(new Shoe()
+            {
+                Style = Style.Loafers,
+                Color = "Biały"
+            });
+
+            shoeCloset.Add(new Shoe()
+            {
+                Style = Style.Loafers,
+                Color = "Czerwony"
+            });
+
+            shoeCloset.Add(new Shoe()
+            {
+                Style = Style.Wingtips,
+                Color = "Czarny"
+            });
+
+            int numberOfShoes = shoeCloset.Count; //It gonna contains six pair of shoes.
+            foreach (Shoe shoe in shoeCloset) //foreach is loop which is executed(?) for same types (does it make sense?). Anyway i mean that foreach is doing method for all included in loop types. I guess im too sleepy.
+            {
+                shoe.Style = Style.Flipflops;
+                shoe.Color = "Pomarańczowy";
+            }
+
+            shoeCloset.RemoveAt(4); //It removes the given index
+
+            Shoe thirdShoe = shoeCloset[2]; // creating 2 references to our shoeCloset indexes.
+            Shoe secondShoe = shoeCloset[1];
+            shoeCloset.Clear(); // We clear our list totally.
+
+            shoeCloset.Add(thirdShoe); // We just add thirdShoe to list.
+            Console.WriteLine(numberOfShoes.ToString());
+            if (shoeCloset.Contains(secondShoe))
+                Console.WriteLine("Wow, cud się wydarzył!");
+
+
         }
     }
 }
---
+++ LearningEnumerationTypesAndCollectionsV0.5/Shoe.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningEnumerationTypesAndCollectionsV0._5
+{
+    class Shoe
+    {
+        public Style Style;
+        public string Color;
+    }
+    public enum Style
+    {
+        Sneakers,
+        Loafers,
+        Snadals,
+        Flipflops,
+        Wingtips,
+        Clogs,
+    }
+}
